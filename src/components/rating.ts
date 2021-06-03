f3dc97b62877e753d847958a1d3d1d988274ca57
Kuchar Moulod <kuchar@keysolutions.se>
2021-06-03 22:28:32 +0200

    initial commit

--- src/components/rating.ts
+++ src/components/rating.ts
@@ -11,9 +11,9 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
   selector: "rating",
   template: `
     <ion-buttons>
-      <ion-button [disabled]="readonly" fill="clear" [size]="size" *ngFor="let current of [1, 2, 3, 4, 5]; let i = index"
-        (click)="onClick(i + 1)" (mouseover)="hoverRate = i + 1" (mouseleave)="hoverRate = 0">
-        <ion-icon slot="icon-only" name="star" [class.filled]="(i + 1 <= hoverRate || (!hoverRate && i + 1 <= rate))"></ion-icon>
+      <ion-button fill="clear" [size]="size" *ngFor="let current of [1, 2, 3, 4, 5]; let i = index"
+        (click)="onClick($event, i + 1)">
+        <div *ngIf="i === 0" class="rating_button">😠</div><div *ngIf="i === 1" class="rating_button">🙁</div><div *ngIf="i === 2" class="rating_button">😐</div><div *ngIf="i === 3" class="rating_button">😃</div><div *ngIf="i === 4" class="rating_button">🤩</div>
       </ion-button>
     </ion-buttons>
   `,
@@ -24,16 +24,12 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
         justify-content: center;
       }
 
-      [ion-button][disabled] {
-        opacity: 1;
-      }
+      .rating_button { 
+        font-size:30px;opacity:0.4; 
+      } 
 
-      ion-icon {
-        color: gray;
-      }
-
-      ion-icon.filled {
-        color: orange;
+      .rating_button:hover { 
+        font-size:32px;opacity:1; 
       }
     `
   ],
@@ -57,10 +53,17 @@ export class RatingComponent implements ControlValueAccessor {
   hoverRate: number;
   _onChange: Function;
 
-  onClick(rate) {
+  onClick(event, rate) {
     this.rate = rate;
     this.rateChange.emit(this.rate);
     this._onChange(this.rate);
+    event.srcElement.style.opacity = '1';
+
+    var elements = document.getElementsByClassName("rating_button");
+    for (var i = 0; i < elements.length; i++) {
+      if(elements[i] != event.srcElement)
+        elements[i].style.opacity = '0.4';
+    }
   }
 
   writeValue(value: any): void {
